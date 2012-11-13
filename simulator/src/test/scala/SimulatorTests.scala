a957b9cb2f67d763d69653e001a940929e95e38d
Chris Lindholm <chris.lindholm@gmail.com>
2012-11-12 23:44:55 -0700

    Changed distance calculation test to use more general calcDistance function.

--- simulator/src/test/scala/SimulatorTests.scala
+++ simulator/src/test/scala/SimulatorTests.scala
@@ -1,14 +1,16 @@
 import simulator._
-import simulator.types._
 import org.scalatest.FunSuite
 
+/*
+ * Things to test:
+ * - works for positive and negative degree values
+ * - works for some minimum distances
+ * - works for some maximum distance
+ */
+
 class SimulatorTests extends FunSuite {
-  test("A source and sensor at the same spot have zero distance.") {
-    // TODO: This isn't ideal, because we have to care about the other
-    // irrelevant aspects of the source and sensor.
-    val source = Source(1, 2, 3)
-    val sensor = Sensor(1, 2)
-    val distance = Simulation.calcDistance(source, sensor)
+  test("A two things at the same spot have zero distance.") {
+    val distance = Simulation.calcDistance((1.0,1.0), (1.0,1.0))
     assert(distance === 0)
   }
 }

25d0706077130dfb690550375bc1ae4f1e8b39fa
Chris Lindholm <chris.lindholm@gmail.com>
2013-02-19 22:28:49 -0700

    Serves KML data from a MongoDB database.

--- KmlServer/src/main/scala/KmlServer.scala
+++ KmlServer/src/main/scala/KmlServer.scala
@@ -15,14 +15,20 @@ object KmlServer extends App with SprayCanHttpServerApp {
   newHttpServer(service) ! Bind(interface = "localhost", port = 8080)
 
   // TODO: Move elsewhere.
-  def getEverything: MongoCursor = {
+  // This is here because there is some sort of conflict with Spray.
+  // It tries to do some implicit typeclass magic which breaks the MongoDB
+  // stuff. This may be helped by limiting what is being imported from Spray.
+  def getEverything: Seq[DBObject] = {
     // TODO: Get host and port from config file.
     val client = MongoClient("localhost", 27017)
     // TODO: Get db name and collection name from config file.
     val collection = client("gunshot")("detections") 
-    val cursor = collection.find()
+    // This is strict! If there are too many things, it will break!
+    // It would be much better to do this in a lazy way.
+    val stuff = collection.find().toSeq
     client.close()
-    return cursor
+    // TODO: Don't want to have to use return statement.
+    return stuff
   }
 }
 
@@ -33,7 +39,7 @@ class KmlServerActor extends Actor with KmlServerThing {
 }
 
 trait KmlServerThing extends HttpService {
-  val route = path("") {
+  val route = path("a" / IntNumber) { _ =>
     get {
       respondWithMediaType(`application/vnd.google-earth.kml+xml`) {
         complete {
--- KmlServer/src/main/scala/generator/KmlGenerator.scala
+++ KmlServer/src/main/scala/generator/KmlGenerator.scala
@@ -1,28 +1,24 @@
 package kmlserver.generator
 
-import com.mongodb.casbah.MongoCursor
+import com.mongodb.casbah.Imports.DBObject
 import scala.xml.Node
 
 object KmlGenerator {
-  implicit object MongoDbKmlGenerator extends KmlGenerator[MongoCursor] {
-    def toKml(obj: MongoCursor): Node = {
+  implicit object MongoDbKmlGenerator extends KmlGenerator[Seq[DBObject]] {
+    def toKml(stuff: Seq[DBObject]): Node = {
       // Can't do XML processing instructions.
       //<?xml version="1.0" encoding="UTF-8"?>
+      // TODO: Add expiration tag.
       <kml xmlns="http://www.opengis.net/kml/2.2">
-        <Placemark>
-          <name>Simple placemark</name>
-          <description>Attached to the ground. Intelligently places itself at the height of the underlying terrain.</description>
-          <Point>
-            <coordinates>40.006605,-105.263695,0</coordinates>
-          </Point>
-        </Placemark>
+        {stuff map { obj  =>
+          <Placemark>
+            <name>Detection</name>
+            <Point>
+              <coordinates>{obj.get("lat")},{obj.get("lon")},0</coordinates>
+            </Point>
+          </Placemark>
+        }}
       </kml>
-
-      /*
-       * But really...
-       * Need to go through each thing in the cursor and convert the
-       * relevant bits into KML.
-       */
     }
   }
 

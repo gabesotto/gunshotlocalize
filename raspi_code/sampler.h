d87f707288b0c8b77b116cc2bfb85a7c94c80741
Erik Kahn <erikekahn@gmail.com>
2013-03-07 08:09:01 -0800

    Worked on definitions of microphone sampling functions

--- raspi_code/sampler.h
+++ raspi_code/sampler.h
@@ -6,6 +6,14 @@
  * we've detected a gunshot. Pew pew.
  */
 
+//TODO: These 4 should NOT be visible to other modules.
 void setup_mic();
 void test_func();
+void listen();
 void cleanup_mic();
+
+// This function will create a new thread for exclusively monitoring the microphone. When
+// a gunshot is actually detected, the given callback will be called :)
+void listenForGunshots(void *callback);
+// Because a new thread is created, this should be called to kindly kill said thread.
+void stopListening();

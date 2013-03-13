00db2f2a84651e13d27e808a1af92cef7ecac2e2
Erik Kahn <erikekahn@gmail.com>
2013-03-13 13:34:11 -0700

    Added code so program could determine MAC address of interface. Modified CONFIG file to reflect the fact that you may choose an interface. Upon hearing a gunshot, program will actually contact the server.

--- raspi_code/main.c
+++ raspi_code/main.c
@@ -20,36 +20,75 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include <pthread.h>
+#include <linux/if.h>
 #include "loc.h"
 #include "sampler.h"
+
 
 #define DEBUG_MSG(x, ...) (printf("DEBUG: " x, ##__VA_ARGS__))
 
 double lat,lon;
 static char *config_file = "./CONFIG";
-
 unsigned short port;
 char hostname[64];
-
+unsigned char mac_addr[6];
 /* This will actually be global, and used for debugging. */
 bool debug = false;
+
+
+// Helper function that determines that MAC address of an intrface.
+void detMACAddr(unsigned char *result, char *interface)
+{
+	struct ifreq dev;
+	int sock;
+
+	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+
+	strcpy(dev.ifr_name, interface);
+	if (ioctl(sock, SIOCGIFHWADDR, &dev) != 0)
+	{
+		fprintf(stderr, "ERROR (FATAL): CANNOT DETERMINE MAC ADDRESS OF '%s'\n", interface);
+		exit(1);
+	}
+
+	close(sock);
+
+	memcpy(result, dev.ifr_addr.sa_data, 6);
+}
 
 // This is a function that should be called from the gunshot listener. It is
 // only called upon the detection of a gunshot.
 void gunshot_handler()
 {
 	struct sockaddr_in sockaddr;
+	struct timeval tv;
 	int sock; 
+	static char buffer[30];
+	double cur_time;
 	
-	// Need to send results to chris. Make sure we have GPS coords. Don't forget
-	// to sleep for ~5 seconds. We don't want to pick up the same gunshot noise.
+
+	memset(&tv, 0, sizeof(struct timeval));
+	gettimeofday(&tv, NULL);
+
+
+	cur_time = (double)tv.tv_sec + ((double)tv.tv_usec/(double)1.0E6);
+
+	*((double *)(&buffer[0])) = cur_time;
+	*((double *)(&buffer[8])) = lat;
+	*((double *)(&buffer[16])) = lon;
+	memcpy((void *)(&buffer[24]), mac_addr, 6);
 
 	if (debug)
-		DEBUG_MSG("I've detected a gunshot! Contacting %s on port %d...\n", hostname, port);
+	{
+		printf("\n****\n");
+		DEBUG_MSG("GUNSHOT occoured at %lf, coords %lf,%lf\n", cur_time, lat, lon);
+	}
 
 	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	memset(&sockaddr, 0, sizeof(struct sockaddr_in));
@@ -63,14 +102,18 @@ void gunshot_handler()
 	if (connect(sock, (struct sockaddr *)&sockaddr, sizeof(struct sockaddr_in)) < 0)
 	{
 		fprintf(stderr, "ERROR: Failed to contact server! Could not open TCP connection!\n");
+		close(sock);
+		return;
 	}
 
 
-	// TODO: Need to send().
+	send(sock, buffer, sizeof(buffer), 0);
 
 	close(sock);
 
+	if (debug) DEBUG_MSG("Waiting to start listening again...\n");
 	sleep(5);
+	if (debug) DEBUG_MSG("Now listening for gunshots again!\n");
 }
 
 void load_config()
@@ -99,7 +142,6 @@ void load_config()
 		if (strcmp(config_key, "PORT") == 0)
 		{
 			sscanf(config_value, "%hd", &port);
-
 			continue;
 		}
 
@@ -108,6 +150,12 @@ void load_config()
 	     memset(hostname, 0, sizeof(hostname));
 		  strcpy(hostname, config_value);
 		  continue;
+		}
+
+		if (strcmp(config_key, "INTERFACE") == 0)
+		{
+			detMACAddr(mac_addr, config_value);
+			continue;
 		}
 
 	}
@@ -147,8 +195,7 @@ int main(int argc, char **argv)
 	// And load the configuration file, or we aren't going to be doing much!
 	load_config();
 
-
-
+	gunshot_handler();
 	// No time to waste. Get cracking on listening to that gunshot.
 	pthread_create(&thread, NULL, &listenForGunshots, &gunshot_handler);
 
